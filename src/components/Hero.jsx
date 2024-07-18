914f64e02ff502a01aced288bff6fd1f0a143013
Tinhuynh211 <tinhuynh211@gmail.com>
2024-07-18 18:41:55 +0700

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import heroImage from "./images/heroImage.png";
-import ayroui from "./images/brands/ayroui.svg";
-import graygrids from "./images/brands/graygrids.svg";
-import uideck from "./images/brands/uideck.svg";
-import backgroundImage from "./images/background.png"; // adjust the path as needed
-import { useAnimation, useInView } from "framer-motion";
 import { Col, Row } from "antd";
-
+import heroImage from "./images/heroImage.png";
+import backgroundImage from "./images/background.png";
 
 const Hero = () => {
   return (
-    
     <div className="overflow-x-hidden bg-gradient-to-br from-blue-100 relative">
       {/* New background image */}
       <img
@@ -21,9 +14,9 @@ const Hero = () => {
         className="absolute inset-0 w-full h-full object-cover z-0 opacity-30 filter"
       />
       <Row className="relative z-10 pb-20 mt-5">
-        <Col  span={12}>
+        <Col xs={24} sm={12}>
           <div className="text-container">
-            <h1 className="responsive-heading ">Chào mừng đến</h1>
+            <h1 className="responsive-heading">Chào mừng đến</h1>
             <span className="company-name font-bromega-bold-italic">TYF Company</span>
             <p className="p-heading">
               Xin chào và cảm ơn bạn đã dành thời gian khám phá về công ty TYF của
@@ -32,27 +25,23 @@ const Hero = () => {
               con trẻ của bạn.
             </p>
             <ul className=" mt-5">
-                    <li >
-                      <Link
-                        to="#"
-                        className="button"
-                      >
-                        Bắt đầu
-                      </Link>
-                    </li>
-                  </ul>
+              <li>
+                <Link to="#" className="button">
+                  Bắt đầu
+                </Link>
+              </li>
+            </ul>
           </div>
         </Col>
-        <Col span={12}>
-        <div>
-        <img
-            src={heroImage}
-            alt="hero"
-            className="max-w-full h-auto object-cover rounded-lg pt-20 img"
-            style={{ width: "100%", maxWidth: "400px", margin: "0 auto" }}
-          />
-        </div>
-          
+        <Col xs={24} sm={12}>
+          <div>
+            <img
+              src={heroImage}
+              alt="hero"
+              className="max-w-full h-auto object-cover rounded-lg pt-20 img"
+              style={{ width: "100%", maxWidth: "400px", margin: "0 auto" }}
+            />
+          </div>
         </Col>
       </Row>
     </div>
